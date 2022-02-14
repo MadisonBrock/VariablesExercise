b682f8511bc20b3da2dec116a558fa8c457c585b
Madison <madison.brock03@gmail.com>
2022-02-14 13:47:08 -0600

    Completed exercise, tried completing bonus

---
+++ script.js
@@ -0,0 +1,28 @@
+// Exercise 
+
+// Value that will not change is a const
+
+num = 20
+console.log(num);
+
+num = 15; 
+console.log(num);
+
+num = num + 7;
+console.log(num);
+
+num--;
+console.log(num);
+
+num = "This is not a number";
+console.log(num);
+
+let blank
+console.log(blank);// Undefined
+
+// Bonus
+const money=quarter = 0.25, dime = 0.10, nickel = 0.05,  penny = 0.01;
+console.log(money);
+
+let letters = "qwertyuiop";
+console.log(letters.length[9]);
